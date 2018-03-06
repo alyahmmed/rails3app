59dbd9ee848bec56cc944f2dda626746221da978
Ali Mohamed <ali.ahmmed.mohamed@gmail.com>
2018-03-06 04:05:14 -0800

    fix rake errors

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-MyApp::Application.config.secret_token = '3ee7ec38b746fb6d1b5b70e85c7bdb46e148dfd7aa32ccb21c9608ece84bcc190762c8548296cb158f33e6ea926d803da011cb14bb2a7f698ed2a2686cd6693c'
+Rails.application.config.secret_token = '3ee7ec38b746fb6d1b5b70e85c7bdb46e148dfd7aa32ccb21c9608ece84bcc190762c8548296cb158f33e6ea926d803da011cb14bb2a7f698ed2a2686cd6693c'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-MyApp::Application.config.session_store :cookie_store, :key => '_myApp_session'
+Rails.application.config.session_store :cookie_store, :key => '_myApp_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
